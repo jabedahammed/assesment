201069048468580b06d53563c3f03ceb0e4bcf14
Jabed <git@pixelsolutionz.in>
2022-04-12 09:06:23 +0530

    modify json and collections

--- js/index.js
+++ js/index.js
@@ -1,33 +1,51 @@
 document.addEventListener('DOMContentLoaded', function(){
     fetch("./data/first_collection.json").then(res=>res.json()).then(res=>{
-        fetch("./data/data.json").then(res=>res.json()).then(res=>{
-            let itemclass = document.getElementById("main-body")
-            let str = ""
-            res.forEach((item,i)=>{
-                str += `<div class="item-child">
-                    <div class="base-img"><img src="./images/${item.imageUrl}" /></div>
-                    <div class="description">
-                        <h2>${item.title}</h2>
-                        <div>${item.intro}</div>
-                        <div class="icon-svg">
-                            <div>
-                                <img src="icons/clock-solid.svg" alt="blabla_is_working">
-                                <span>${item.publishedOn}</span>
+        let collection = res.find(item=>{
+            return item.collectiontype == "landing"
+        })
+        if(collection){
+            fetch("./data/data.json").then(res=>res.json()).then(res=>{
+                if(res && Array.isArray(res)){
+                    let finalData = res.filter(item=>{
+                        if(item.collectionid == collection.collectionid){
+                            return true
+                        }
+                        return false;
+                    })
+                    let itemclass = document.getElementById("main-body")
+                    let str = ""
+                    finalData.forEach((item,i)=>{
+                        str += `<div class="item-child">
+                            <div class="base-img"><img src="./images/${item.imageUrl}" /></div>
+                            <div class="description">
+                                <h2>${item.title}</h2>
+                                <div>${item.intro}</div>
+                                <div class="icon-svg">
+                                    <div>
+                                        <img src="icons/clock-solid.svg" alt="clock-soldi">
+                                        <span>${item.publishedOn}</span>
+                                    </div>
+                                    <div>
+                                        <img src="icons/comment-alt-regular.svg" alt="comment-alt-regular">
+                                        <span>${item.numberOfComment}</span>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <img src="icons/comment-alt-regular.svg" alt="blabla_is_working">
-                                <span>${item.numberOfComment}</span>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    
-                </div>`
+                            
+                            
+                        </div>`
+                    })
+                    itemclass.innerHTML = str;
+                }else{
+                    alert("Sorry some unknown error happened")
+                }
+                
+            }).catch(err=>{
+                alert("Sorry some unknown error happened")
             })
-            itemclass.innerHTML = str;
-        }).catch()
+        }
     }).catch(err=>{
-
+        alert("Sorry some unknown error happened")
     })
     
 }, false);
